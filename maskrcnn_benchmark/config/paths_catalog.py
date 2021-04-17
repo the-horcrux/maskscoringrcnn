515e6d38d331f811f3384079c7570b725f825984
nishagr <nisheethagrawal9822@gmail.com>
2021-04-18 04:39:07 +0530

    Working Code

--- maskrcnn_benchmark/config/paths_catalog.py
+++ maskrcnn_benchmark/config/paths_catalog.py
@@ -5,37 +5,25 @@ import os
 
 
 class DatasetCatalog(object):
-    DATA_DIR = "datasets"
+    DATA_DIR = "leaf_dataset"
     DATASETS = {
-        "coco_2014_train": (
-            "coco/train2014",
-            "coco/annotations/instances_train2014.json",
-        ),
-        "coco_2014_val": ("coco/val2014", "coco/annotations/instances_val2014.json"),
-        "coco_2014_test": (
-            "coco/test2014",
-            "coco/annotations/image_info_test2014.json",
-        ),
-        "coco_2014_valminusminival": (
-            "coco/val2014",
-            "coco/annotations/instances_valminusminival2014.json",
-        ),
+        "train": ("train", "annotations/train.json"),
+        "val": ("val", "annotations/val.json"),
+        "test": ("test", "annotations/test.json"),
     }
 
     @staticmethod
     def get(name):
-        if "coco" in name:
-            data_dir = DatasetCatalog.DATA_DIR
-            attrs = DatasetCatalog.DATASETS[name]
-            args = dict(
-                root=os.path.join(data_dir, attrs[0]),
-                ann_file=os.path.join(data_dir, attrs[1]),
-            )
-            return dict(
-                factory="COCODataset",
-                args=args,
-            )
-        raise RuntimeError("Dataset not available: {}".format(name))
+        data_dir = DatasetCatalog.DATA_DIR
+        attrs = DatasetCatalog.DATASETS[name]
+        args = dict(
+            root=os.path.join(data_dir, attrs[0]),
+            ann_file=os.path.join(data_dir, attrs[1]),
+        )
+        return dict(
+            factory="COCODataset",
+            args=args,
+        )
 
 
 class ModelCatalog(object):
